e61b227fda7f4d29aba5c3a42a2d63c51b679249
Sasu <sasuolander@yahoo.com>
2020-03-14 16:59:14 +0200

    m

---
+++ src/app/editItem/editItem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editItem',
+  templateUrl: './editItem.component.html',
+  styleUrls: ['./editItem.component.css']
+})
+export class EditItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
