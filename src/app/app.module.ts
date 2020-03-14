2907f20496df4e4d5b58eb4186516ab16d39776c
Sasu <sasuolander@yahoo.com>
2020-03-14 16:58:19 +0200

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ItemsComponent } from './items/items.component';
+import { EditItemComponent } from './editItem/editItem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    ItemsComponent,
+    EditItemComponent,
+    NgbModule
   ],
   imports: [
     BrowserModule
